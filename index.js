d6322caebe171507776f0d029ffe0e92a6e55dc2
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2022-05-20 15:37:28 +0200

    FEAT HIT INVADERS

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ class Projectile {
     this.position = position;
     this.velocity = velocity;
 
-    this.radius = 3;
+    this.radius = 4;
   }
   draw() {
     c.beginPath();
@@ -200,8 +200,31 @@ const animate = () => {
 
   grids.forEach((grid) => {
     grid.update();
-    grid.invaders.forEach((invader) => {
+    grid.invaders.forEach((invader, i) => {
       invader.update({ velocity: grid.velocity });
+      projectiles.forEach((projectile, j) => {
+        if (
+          projectile.position.y - projectile.radius <=
+            invader.position.y + invader.height &&
+          projectile.position.x + projectile.radius >= invader.position.x &&
+          projectile.position.x - projectile.radius <= invader.position.x + invader.width &&
+          projectile.position.y + projectile.radius >= invader.position.y
+        ) {
+          setTimeout(() => {
+            const invaderFound = grid.invaders.find(
+              (invader2) => invader2 === invader
+            );
+            const projectileFound = projectiles.find(
+              (projectile2) => projectile2 === projectile
+            );
+
+            if (invaderFound && projectileFound) {
+              grid.invaders.splice(i, 1);
+              projectiles.splice(j, 1);
+            }
+          }, 0);
+        }
+      });
     });
   });
 
@@ -223,7 +246,7 @@ const animate = () => {
   if (frames % randomInterval === 0) {
     grids.push(new Grid());
     randomInterval = Math.floor(Math.random() * 500 + 500);
-    frames = 0
+    frames = 0;
   }
   frames++;
 };
