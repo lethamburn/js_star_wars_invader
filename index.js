fd372d23f4ded1ad25e3ffc41e57c16e1d373cb9
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2022-05-20 15:01:01 +0200

    MOVE GRID

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ class Player {
     const image = new Image();
     image.src = "./assets/xwing.png";
     image.onload = () => {
-      const scale = 0.1;
+      const scale = 0.08;
       this.image = image;
       this.width = image.width * scale;
       this.height = image.height * scale;
@@ -84,7 +84,7 @@ class Projectile {
 
 //INVADER
 class Invader {
-  constructor() {
+  constructor({ position }) {
     this.velocity = {
       x: 0,
       y: 0,
@@ -93,13 +93,13 @@ class Invader {
     const image = new Image();
     image.src = "./assets/tie.png";
     image.onload = () => {
-      const scale = 0.35;
+      const scale = 0.8;
       this.image = image;
       this.width = image.width * scale;
       this.height = image.height * scale;
       this.position = {
-        x: canvas.width / 2 - this.width / 2,
-        y: canvas.height / 2,
+        x: position.x,
+        y: position.y,
       };
     };
   }
@@ -114,18 +114,62 @@ class Invader {
     );
   }
 
-  update = () => {
+  update = ({ velocity }) => {
     if (this.image) {
       this.draw();
-      this.position.x += this.velocity.x;
-      this.position.y += this.velocity.y;
+      this.position.x += velocity.x;
+      this.position.y += velocity.y;
     }
   };
 }
 
+class Grid {
+  constructor() {
+    this.position = {
+      x: 0,
+      y: 0,
+    };
+    this.velocity = {
+      x: 3,
+      y: 0,
+    };
+    this.invaders = [];
+
+    const rows = Math.floor(Math.random() * 5 + 2);
+    const columns = Math.floor(Math.random() * 10 + 5);
+
+    this.width = columns * 30;
+
+    for (let x = 0; x < columns; x++) {
+      for (let y = 0; y < rows; y++) {
+        this.invaders.push(
+          new Invader({
+            position: {
+              x: x * 30,
+              y: y * 30,
+            },
+          })
+        );
+      }
+    }
+  }
+
+  update() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    this.velocity.y = 0
+
+    if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
+      this.velocity.x = -this.velocity.x;
+      this.velocity.y = 30
+    }
+  }
+}
+
 const player = new Player();
 const projectiles = [];
-const invader = new Invader();
+const grids = [new Grid()];
 const keys = {
   a: {
     pressed: false,
@@ -142,7 +186,6 @@ const animate = () => {
   requestAnimationFrame(animate);
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
-  invader.update();
   player.update();
   projectiles.forEach((projectile, index) => {
     if (projectile.position.y + projectile.radius <= 0) {
@@ -150,6 +193,13 @@ const animate = () => {
     } else {
       projectile.update();
     }
+  });
+
+  grids.forEach((grid) => {
+    grid.update();
+    grid.invaders.forEach((invader) => {
+      invader.update({ velocity: grid.velocity });
+    });
   });
 
   if (keys.a.pressed && player.position.x >= 0) {
